e4e15f153aca99550f2240c97b2667888862181a
ridsor <ridsorgamerz@gmail.com>
2025-05-09 06:08:32 +0900

    created leading-page part1

---
+++ resources/views/components/pages/home/category.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <div class="container py-5">
+        <div class="row">
+            @foreach ($categories as $category)
+                <div class="col-md-3">
+                    <div class="d-flex flex-column align-items-center justify-content-center text-center">
+                        <img src="{{ $category->icon }}" style="width: 100px; aspect-ratio: 1 / 1;" />
+                        <div>{{ $category->name }}</div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <script src="https://kit.fontawesome.com/0c8762722d.js" crossorigin="anonymous"></script>
+    <title>{{ $title ?? config('app.name') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/js/app.js','resources/css/app.css','resources/css/bootstrap.css'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <script src="https://kit.fontawesome.com/0c8762722d.js" crossorigin="anonymous"></script>
 
-        {{-- CSS --}}
-        @stack('styles')
-    </head>
-    <body>
-        {{ $slot }}
-        @stack('scripts')
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/css/bootstrap.css'])
+
+    {{-- CSS --}}
+    @stack('styles')
+</head>
+
+<body>
+    @include('layouts.header')
+    @yield('content')
+
+    {{-- JS --}}
+    @stack('scripts')
+</body>
+
 </html>
