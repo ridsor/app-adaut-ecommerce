56182f2d7f030b1159d2672c04a99e9c70c315d1
Ridsor <ridsorgamerz@gmail.com>
2025-05-12 17:47:54 +0900

    created leading-page part3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,59 @@
 import "./bootstrap";
 
 import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
+
+Alpine.plugin(persist);
 
 window.Alpine = Alpine;
 
 Alpine.store("globalState", {
     formattedPrice,
+});
+
+Alpine.store("cart", {
+    select: Alpine.$persist([]),
+    items: Alpine.$persist([
+        {
+            product_id: 1,
+            quantity: 1,
+            image: "https://themewagon.github.io/FoodMart/images/product-thumb-1.png",
+            price: 15000,
+            name: "Produk",
+        },
+    ]),
+    total: Alpine.$persist(0),
+    update(newitem, quantity) {
+        if (quantity > 0) {
+            this.items = this.items.map((item) => {
+                if (item.id === newitem.id) {
+                    item.quantity = quantity;
+                }
+                return item;
+            });
+        }
+    },
+    add(newItem) {
+        this.items.push({ ...newItem });
+    },
+    total(item) {
+        const result = this.items.reduce((acc, cur) => {
+            acc + cur.price * cur.quantity;
+        }, 0);
+        return result;
+    },
+    remove(id) {
+        const cartItem = this.items.find((item) => item.id === newItem.id);
+        if (cartItem) {
+            this.items = this.items.filter((item) => {
+                if (item.id !== id) {
+                    this.total -= item.price * item.quantity;
+                    return false;
+                }
+                return true;
+            });
+        }
+    },
 });
 
 Alpine.start();
---
+++ resources/views/components/loading.blade.php
@@ -0,0 +1,11 @@
+<div id="loadingAnimation"></div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.5/lottie.min.js'></script>
+<script>
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('loadingAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path:'/assets/animations/loading.json' // lottie file path
+  })
+</script>
