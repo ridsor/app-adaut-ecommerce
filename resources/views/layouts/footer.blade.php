9441b19ef7421c5ce83023aa48a52d3e7465ffe4
Ridsor <ridsorgamerz@gmail.com>
2025-05-14 17:46:31 +0900

    revisi footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,43 +1,69 @@
 <footer>
     <div class="container">
         <div class="py-5">
-            <div class="d-flex flex-column">
-                <div class="logo">
-                    <span class="fs-4 fw-bold">ADAUT</span>
+            <div class="d-flex gap-2 justify-content-between flex-column flex-md-row">
+                <div>
+                    <div class="logo">
+                        <span class="fs-4 fw-bold mb-3">ADAUT</span>
+                    </div>
                 </div>
-                <div class="social-media d-flex align-items-center gap-2 mt-5">
-                    <a href="#">
-                        <div class="btn btn-outline-light px-3">
-                            <i class="fa-brands fa-facebook-f" style="font-size: 13px"></i>
+                <div style="flex: 1">
+                    <div class="d-flex flex-column gap-2 ms-0 ms-md-4">
+                        <div>
+                            <a class="text-decoration-none text-secondary" href="{{ route('search') }}?sort=bestsellers">
+                                Penjualan Terbaik
+                            </a>
                         </div>
-                    </a>
-                    <a href="#">
-                        <div class="btn btn-outline-light px-3">
-                            <i class="fa-brands fa-x-twitter"></i>
+                        <div>
+                            <a class="text-decoration-none text-secondary" href="{{ route('search') }}?sort=latest">
+                                Rilisan Baru
+                            </a>
                         </div>
-                    </a>
-                    <a href="#">
-                        <div class="btn btn-outline-light px-3">
-                            <i class="fa-brands fa-pinterest"></i>
+                        <div>
+                            <a class="text-decoration-none text-secondary"
+                                href="{{ route('search') }}?sort=lowest_price">
+                                Harga Termurah
+                            </a>
                         </div>
-                    </a>
-                    <a href="#">
-                        <div class="btn btn-outline-light px-3">
-                            <i class="fa-brands fa-instagram"></i>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div class="btn btn-outline-light px-3">
-                            <i class="fa-brands fa-youtube"></i>
-                        </div>
-                    </a>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center flex-column mt-5 mt-md-0">
+                    <p class="mb-4">
+                        <i class="fa-solid fa-phone"></i> +62 823 4567 8901
+                    </p>
+                    <div class="social-media d-flex align-items-center gap-2">
+                        <a href="#">
+                            <div class="btn btn-outline-light px-3">
+                                <i class="fa-brands fa-facebook-f" style="font-size: 13px"></i>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="btn btn-outline-light px-3">
+                                <i class="fa-brands fa-x-twitter"></i>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="btn btn-outline-light px-3">
+                                <i class="fa-brands fa-pinterest"></i>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="btn btn-outline-light px-3">
+                                <i class="fa-brands fa-instagram"></i>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="btn btn-outline-light px-3">
+                                <i class="fa-brands fa-youtube"></i>
+                            </div>
+                        </a>
+                    </div>
+
                 </div>
             </div>
         </div>
-        <div>
-            <p>
-                © 2023 Adaut. All rights reserved.
-            </p>
-        </div>
+        <p class="text-center">
+            © 2023 Adaut. All rights reserved.
+        </p>
     </div>
 </footer>
