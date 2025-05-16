77e24766100f0ea1e66514c1c5ce4df53eb57b50
Ridsor <ridsorgamerz@gmail.com>
2025-05-16 16:29:31 +0900

    update google auth

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Throwable;
 
@@ -28,9 +29,9 @@ class GoogleAuthController extends Controller
             // Get the user information from Google
             $user = Socialite::driver('google')->user();
         } catch (Throwable $e) {
-            return redirect('/')->with('error', 'Google authentication failed.');
+            return redirect(route('home', absolute: false))->with('error', 'Google authentication failed.');
         }
-
+        
         // Check if the user already exists in the database
         $existingUser = User::where('email', $user->email)->first();
 
@@ -38,16 +39,31 @@ class GoogleAuthController extends Controller
             // Log the user in if they already exist
             Auth::login($existingUser);
         } else {
-            // Otherwise, create a new user and log them in
-            $newUser = User::updateOrCreate([
-                'email' => $user->email
-            ], [
-                'name' => $user->name,
-                'image' => $user->image,
-                'password' => bcrypt(Str::random(16)),
-                'email_verified_at' => now()
-            ]);
-            Auth::login($newUser);
+            try {
+                DB::beginTransaction();
+                
+                // Otherwise, create a new user and log them in
+                $newUser = User::updateOrCreate([
+                    'email' => $user->email
+                ], [
+                    'name' => $user->name,
+                    'password' => bcrypt(Str::random(16)),
+                    'email_verified_at' => now()
+                ]);
+                
+                $newUser->profile()->updateOrCreate([
+                    'user_id' => $user->id
+                ],[
+                    'image' => $user->avatar,
+                ]);
+
+                Auth::login($newUser);
+
+                DB::commit();
+            } catch (Throwable $e) {
+                DB::rollBack();
+                return redirect(route('home', absolute: false))->with('error', 'Google authentication failed.');
+            }
         }
 
         // Redirect the user to the dashboard or any other secure page
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'image',
+        'phone_number',
+        'gender',
+        'date_of_birth'
+    ];
+
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
