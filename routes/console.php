3a443e801e078f5ab52d32e79288b689305709d0
ridsor <ridsorgamerz@gmail.com>
2025-06-12 14:53:51 +0900

    mengerjakan fitur cronjob untuk softdeleted

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('data:delete-soft-deletes')->weekly();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
